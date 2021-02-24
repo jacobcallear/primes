cd0f1fa8f843c95a370a1e5b4f77e0bf222a7fbd
Jacob Callear <jacob18@callear.net>
2021-02-24 16:04:58 +0000

    Split code into lib.rs file

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,9 @@
+use primes;
+
 fn main() {
     for i in 1..100 {
-        if is_prime(i) {
+        if primes::is_prime(i) {
             println!("{} is prime", i);
-        }
-    }
-}
-
-fn is_prime(number: u64) -> bool {
-    match number {
-        1 => false,
-        2 | 3 => true,
-        _ => {
-            let test_divisors = Divisors::new(number);
-            for divisor in test_divisors {
-                if number % divisor == 0 {
-                    return false
-                }
-            }
-            true
-        }
-    }
-}
-
-struct Divisors {
-    max_divisor: u64,
-    minus: bool,
-    n: u64,
-    divisor: u32,
-}
-
-impl Divisors {
-    fn new(number: u64) -> Divisors {
-        Divisors {
-            max_divisor: (number as f64).sqrt() as u64 + 1,
-            minus: true,
-            n: 1,
-            divisor: 2,
-        }
-    }
-}
-
-impl Iterator for Divisors {
-    type Item = u64;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.divisor == 2 {
-            self.divisor = 3;
-            return Some(2);
-        } else if self.divisor == 3 {
-            self.divisor = 4;
-            return Some(3);
-        };
-        let next_term = if self.minus {
-            self.minus = false;
-            6 * self.n - 1
-        } else {
-            self.minus = true;
-            self.n += 1;
-            6 * self.n + 1
-        };
-
-        if next_term <= self.max_divisor {
-            Some(next_term)
-        } else {
-            None
         }
     }
 }
