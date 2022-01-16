b56457ea22519085e19c6520461ba67318765404
Jacob Callear <jacob18@callear.net>
2022-01-16 17:32:54 +0000

    refactor: Use more specific imports (minor change)

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
+use primes::{is_prime, parse_range};
 use std::env;
 use std::process;
 
 fn main() {
-    let range = primes::parse_range(env::args()).unwrap_or_else(|err| {
+    let range = parse_range(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     for i in range {
-        if primes::is_prime(i) {
+        if is_prime(i) {
             println!("{}", i);
         }
     }
